7a235d8b82e5d09f4452958e1164e17b1e8e8757
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-09-30 08:51:30 -0300

    Função das imagens automáticas - funcionando com exceção das imagens iguais

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const imagens = [
     "./img/tenjhotenge.jpg",
 ]
 
+const limparId = (urlImagem) => urlImagem
+    .split("/")[2]
+    .split(".")[0]
+    .replace(" ", "-")
+    .toLowerCase()
+
 const criarItem = (urlImagem) => {
 
     const container = document.querySelector(".galeria-container")
@@ -72,4 +78,42 @@ const criarItemModal = (urlImagem) => {
     console.log(urlImagem)
 }
 
+
+const criarSlide = (urlImagem, indice, arr) => {
+    const container = document.querySelector(".slide-container")
+    const slide = document.createElement("div")
+    slide.classList.add("slide")
+    slide.id = limparId(urlImagem)
+
+    const indiceAnterior = indice > 0 ? indice - 1 : arr.length - 1 
+    const idAnterior = limparId(arr[indiceAnterior])
+
+    const indiceProximo = indice < arr.length - 1 ? indice + 1 : 0
+    const idProximo = limparId(arr[indiceProximo])
+
+    // let indiceAnterior
+
+    // if (indice > 0){
+    //     indiceAnterior = indice - 1
+    // }
+    // else{
+    //     indiceAnterior = arr.length - 1
+    // }
+    
+
+    slide.innerHTML = `
+    <div class="imagem-container">
+        <a href="#" class="fechar">&#128473;</a>
+        <a href="#${idAnterior}" class="navegacao anterior">&#171;</a>
+        <img src="${urlImagem}" alt="">
+        <a href="#${idProximo}" class="navegacao proximo">&#187;</a>
+    </div>
+`
+    container.appendChild(slide)
+}
+
+const carregarSlides = () => imagens.forEach(criarSlide)
+
+
 carregarImagens()
+carregarSlides()
